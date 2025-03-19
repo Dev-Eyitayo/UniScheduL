9e39a1ef190485338563c38704cd201e9208f968
Eden Iyanda <edeniyanda@gmail.com>
2025-03-19 15:21:16 +0100

    Integrate PublicLayout for public routes in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminLayout from "./components/AdminLayout";
+import PublicLayout from "./components/PublicLayout"; // NEW
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
@@ -11,18 +12,17 @@ import ManageLecturers from "./pages/admin/ManageLecturers";
 import WeeklyTimetable from "./pages/admin/WeeklyTimetable";
 import OptimizedSchedule from "./pages/admin/OptimizedSchedule";
 import GeneratePDF from "./pages/admin/GeneratePDF";
-import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
+        {/* Public Routes */}
+        <Route path="/" element={<PublicLayout> <Home /> </PublicLayout>} />
+        <Route path="/dashboard" element={<PublicLayout> <Dashboard /> </PublicLayout>} />
+        <Route path="/login" element={<PublicLayout> <Login /> </PublicLayout>} />
 
-        {/* Wrap Admin Routes Inside Layout */}
+        {/* Admin Routes (Handled by AdminLayout) */}
         <Route path="/admin/*" element={<AdminLayout> <AdminDashboard /> </AdminLayout>} />
         <Route path="/admin/rooms" element={<AdminLayout> <ManageRooms /> </AdminLayout>} />
         <Route path="/admin/courses" element={<AdminLayout> <ManageCourses /> </AdminLayout>} />
