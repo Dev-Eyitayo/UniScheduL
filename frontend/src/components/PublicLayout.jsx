54a928fd1c3651bc0aa3022af8e1cdf6f7186503
Eden Iyanda <edeniyanda@gmail.com>
2025-03-19 15:26:45 +0100

    Update PublicLayout to adjust main content positioning below the navbar

--- frontend/src/components/PublicLayout.jsx
+++ frontend/src/components/PublicLayout.jsx
@@ -3,11 +3,13 @@ import Navbar from "./Navbar";
 export default function PublicLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Public Navbar (Visible only for public pages) */}
+      {/* Navbar (fixed position) */}
       <Navbar />
       
-      {/* Main content */}
-      <main className="p-6 bg-gray-100 flex-1">{children}</main>
+      {/* Ensure content starts below the navbar */}
+      <main className="pt-16 p-6 bg-gray-100 flex-1">
+        {children}
+      </main>
     </div>
   );
 }
