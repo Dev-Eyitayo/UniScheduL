8eb5d72834fd645a295448095b5e6489ab63ee72
Eden Iyanda <edeniyanda@gmail.com>
2025-03-22 13:46:58 +0100

    Add URL routes for Lecturer, Room, Course, and TimeSlot APIs

--- unischedule_django/unischedule_django/urls.py
+++ unischedule_django/unischedule_django/urls.py
@@ -1,22 +1,23 @@
-"""
-URL configuration for unischedule_django project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Lecturer APIs
+    path('api/lecturers', views.lecturers_view),
+    path('api/lecturers/<int:pk>', views.lecturer_detail),
+
+    # Room APIs
+    path('api/rooms', views.rooms_view),
+    path('api/rooms/<int:pk>', views.room_detail),
+
+    # Course APIs
+    path('api/courses', views.courses_view),
+    path('api/courses/<str:pk>', views.course_detail),
+
+    # TimeSlot APIs
+    path('api/timeslots', views.timeslots_view),
+    path('api/timeslots/<int:pk>', views.timeslot_detail),
 ]
