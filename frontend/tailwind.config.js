d79042acdbb46867373237c9574fcad90a51283c
Ezekiel Eyitayo <ezekieleyitayo2020@gmail.com>
2025-03-23 00:11:01 +0100

    Added loading animation to run algorithm

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+      
+//     },
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -5,10 +19,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      
-    },
+    extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+};
